eaf0b25aa3f284b3172684463a0968caa5f27f18
Scott Harris <scottharris@outlook.com>
2014-08-09 18:33:31 -0400

    shit just works.. done way to much to remember.. so ill push now

---
+++ test/models/user_test.rb
@@ -0,0 +1,44 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "a user should enter a first name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:first_name].empty?
+  end
+
+  test "a user should enter a last name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:last_name].empty?
+  end
+
+  test "a user should enter a profile name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user should have a unique profile name" do
+    user = User.new
+    users(:scott)
+    user.profile_name = users(:scott).profile_name
+    
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user should have a profile name without spaces" do
+    user = User.new
+    users(:scott)
+    user.profile_name = "My Profile with spaces"
+    
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+    assert user.errors[:profile_name].include?("Must be formatted correctly.")
+  end
+end
